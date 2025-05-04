b59ead5ad4965c1e3ab944d17e891b541790dc23
NickolayKiritsa <129768892+NickolayKiritsa@users.noreply.github.com>
2025-05-05 00:57:45 +0300

    fixed language select

--- src/app/common-ui/header/header.component.ts
+++ src/app/common-ui/header/header.component.ts
@@ -1,16 +1,28 @@
 import {Component, inject} from '@angular/core';
 import {Router} from '@angular/router';
+import {CommonModule} from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+CommonModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
  public  menuOpen = false;
+  languages: Language[] = [
+    { code: 'en', name: 'English',  flag: 'https://flagcdn.com/w40/gb.png' },
+    { code: 'ru', name: 'Русский',  flag: 'https://flagcdn.com/w40/ru.png' },
+    { code: 'ro', name: 'Română',    flag: 'https://flagcdn.com/w40/ro.png' },
+  ];
+  public selectedLanguage!: string;
+  public dropdownVisible: boolean = false;
 
- router = inject(Router);
+
+  router = inject(Router);
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
@@ -19,11 +31,22 @@ export class HeaderComponent {
   closeMenu() {
     this.menuOpen = false;
   }
-
+  public toggleLanguageDropdown() {
+    this.dropdownVisible = !this.dropdownVisible;
+  }
 
   setLang(lang: string) {
     console.log('Selected lang:', lang);
 
   }
 
+
+
 }
+
+export interface Language {
+  code: string;
+  name: string;
+  flag: string;
+}
+
